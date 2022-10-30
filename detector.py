31f3f4d48e132069b25a20430f5867d4cb49bba8
Ashok Saravanan <90977640+AshokSaravanan222@users.noreply.github.com>
2022-10-29 21:18:57 -0400

    Update detector.py

--- detector.py
+++ detector.py
@@ -1,3 +1,6 @@
+# Author: Ashok Saravanan
+# Created: 
+
 import cv2
 import numpy as np
 import math
@@ -60,50 +63,5 @@ cv2.imshow('result', crops[1][3])
 cv2.waitKey(0)
 
 
-
-
 #crop3 = crops[1][1][100:150, 100:150]
 #print(crop3)
-
-'''
-cv2.circle(C, (x, y), r, (0, 255, 0), 4)
-cv2.rectangle(C, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-
-
-
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-blur = cv2.GaussianBlur(img_gray, (7, 7), 0)
-thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 7))
-dilate = cv2.dilate(thresh, kernel, iterations=1)
-invert = cv2.bitwise_not(dilate)
-
-circles = cv2.HoughCircles(invert, cv2.HOUGH_GRADIENT, 1.2, 105)
-# ensure at least some circles were found
-if circles is not None:
-    circles = np.round(circles[0, :]).astype("int")
-    print(circles)
-    for (x, y, r) in circles:
-        area = 3.1415926 * r**2
-        if area >= 80000:
-            print(area)
-            cv2.circle(img, (x, y), r, (0, 255, 0), 4)
-            cv2.rectangle(img, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-'''
-
-'''
-
-cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-cnts = sorted(cnts, key=lambda x: cv2.boundingRect(x)[0])
-'''
-
-'''
-for c in cnts:
-    x, y, w, h = cv2.boundingRect(c)
-    if w*h > 1000:
-        print(str(x) + ' ' + str(y) + ' ' + str(x + w) + ' ' + str(y + h) + ' ' + str(w * h))
-        cv2.rectangle(img, (x, y), (x + w, y + h), (36, 255, 12), 2)
-'''
